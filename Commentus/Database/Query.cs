ed24b40fe96af56d6e7e3d53207c8e8274538ac2
trachtt <trachta.tomas@email.cz>
2023-04-17 16:44:20 +0200

    Query nonsense removed

--- Commentus/Database/Query.cs
+++ Commentus/Database/Query.cs
@@ -4,19 +4,15 @@ namespace Commentus.Database
 {
     public static class Query
     {
-        private static string _query;
 
         public static void ExecNonQuery(string query, MySqlConnection connection)
         {
-            _query = query;
-
-            var cmd = new MySqlCommand(_query, connection);
+            var cmd = new MySqlCommand(query, connection);
             cmd.ExecuteNonQuery();
         }
 
         public static MySqlDataReader ExecReader(string query, MySqlConnection connection)
         {
-            _query = query;
 
             var cmd = new MySqlCommand(query, connection);
             return cmd.ExecuteReader();
@@ -24,8 +20,6 @@ namespace Commentus.Database
 
         public static object ExecScalar(string query, MySqlConnection connection)
         {
-            _query = query;
-
             var cmd = new MySqlCommand(query, connection);
             return cmd.ExecuteScalar();
         }
